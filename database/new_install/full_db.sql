3514d20254b1bf047315492c6312b0f6da033661
Vladislav Beluaev <vladislav.beluaev@gmail.com>
2019-11-12 16:33:32 +0300

    no comment

--- database/new_install/full_db.sql
+++ database/new_install/full_db.sql
@@ -19,24 +19,25 @@ USE `avtoelectrikminsk`;
 -- Дамп структуры для таблица avtoelectrikminsk.migrations
 CREATE TABLE IF NOT EXISTS `migrations` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `migration` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы avtoelectrikminsk.migrations: ~4 rows (приблизительно)
+-- Дамп данных таблицы avtoelectrikminsk.migrations: ~5 rows (приблизительно)
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 	(1, '2014_10_12_000000_create_users_table', 1),
 	(2, '2014_10_12_100000_create_password_resets_table', 1),
 	(5, '2019_11_05_182336_create_services_table', 2),
-	(6, '2019_11_05_182815_create_service_types_table', 2);
+	(6, '2019_11_05_182815_create_service_types_table', 2),
+	(8, '2019_11_08_101614_add_columns_img_attribs_to_service_types', 3);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 
 -- Дамп структуры для таблица avtoelectrikminsk.password_resets
 CREATE TABLE IF NOT EXISTS `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   KEY `password_resets_email_index` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -48,15 +49,15 @@ CREATE TABLE IF NOT EXISTS `password_resets` (
 -- Дамп структуры для таблица avtoelectrikminsk.services
 CREATE TABLE IF NOT EXISTS `services` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы avtoelectrikminsk.services: ~0 rows (приблизительно)
+-- Дамп данных таблицы avtoelectrikminsk.services: ~2 rows (приблизительно)
 /*!40000 ALTER TABLE `services` DISABLE KEYS */;
-INSERT INTO `services` (`id`, `title`, `created_at`, `updated_at`) VALUES
+INSERT INTO `services` (`id`, `name`, `created_at`, `updated_at`) VALUES
 	(1, 'диагностика', '2019-11-05 21:35:19', '2019-11-05 21:35:21'),
 	(2, 'ремонт', '2019-11-05 21:35:22', '2019-11-05 21:35:23');
 /*!40000 ALTER TABLE `services` ENABLE KEYS */;
@@ -64,8 +65,10 @@ INSERT INTO `services` (`id`, `title`, `created_at`, `updated_at`) VALUES
 -- Дамп структуры для таблица avtoelectrikminsk.service_types
 CREATE TABLE IF NOT EXISTS `service_types` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `img_src` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'storage/diagnostics/diagnostics_1.jpg',
+  `img_alt` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'diagnostics_1',
   `fk_service` bigint(20) unsigned DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -74,31 +77,31 @@ CREATE TABLE IF NOT EXISTS `service_types` (
   CONSTRAINT `service_types_fk_service_foreign` FOREIGN KEY (`fk_service`) REFERENCES `services` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы avtoelectrikminsk.service_types: ~0 rows (приблизительно)
+-- Дамп данных таблицы avtoelectrikminsk.service_types: ~12 rows (приблизительно)
 /*!40000 ALTER TABLE `service_types` DISABLE KEYS */;
-INSERT INTO `service_types` (`id`, `title`, `description`, `fk_service`, `created_at`, `updated_at`) VALUES
-	(1, 'компьютерная диагностика', 'Используя современное оборудование и методики проводим полную диагностику систем автомобиля.', 1, '2019-11-05 21:46:42', '2019-11-05 21:46:42'),
-	(2, 'диагностика двигателя', 'Диагностика работы элементов систем двигателя автомобиля квалифицированными мастерами, определение: — качества работы\r\nмотора, — неисправностей при запуске мотора, — причин падения мощности, — причин посторонних звуков в моторе и пр.', 1, '2019-11-05 21:46:42', '2019-11-05 21:46:42'),
-	(3, 'диагностика АКПП', 'Проводим полную диагностику автоматической коробки передач автомобиля, устанавливаем наличие или отсутствие проблем в работе коробки, консультируем и при выявлении ошибок подбираем наиболее оптимальное решение проблем с последующим устранением.', 1, '2019-11-05 21:46:42', '2019-11-05 21:46:42'),
-	(4, 'диагностика ABS', 'Антиблокировочная система – важная составляющая системы безопасности автомобиля. Наши мастера произведут качественную диагностику системы с определением необходимости ремонта блока ABS. Позаботьтесь о безопасности заранее.', 1, '2019-11-05 21:46:42', '2019-11-05 21:46:42'),
-	(5, 'диагностика Airbag', 'С помощью современной компьютерной диагностики определяем наличие или отсутствие неисправностей в системе работы подушек безопасности Вашего автомобиля. Ремонт, прошивка, удаление ошибок, восстановление работоспособности.', 1, '2019-11-05 21:46:42', '2019-11-05 21:46:42'),
-	(6, 'диагностика климат контроля', 'При помощи спецоборудования производим считывание системы климат контроля Вашего автомобиля, определяем состав, температуру, влажность воздуха в Авто. При выявлении дефекта системы наши сотрудники подберут наиболее рациональное решение по устранению ошибок, проконсультируют и дадут советы для дальнейшей успешной работы системы. (Устранение климатического дискомфорта в салоне).', 1, '2019-11-05 21:46:42', '2019-11-05 21:46:42'),
-	(7, 'диагностика приборной панели', 'Данный тип компьютерной проверки направлен на считывание, расшифровку и анализ полученных значений приборной панели. Проверка показаний спидометра, тахометра, указателей уровня топлива, температуры и пр. показателей, значения которых выводятся на панель приборов.', 1, '2019-11-05 21:46:42', '2019-11-05 21:46:42'),
-	(8, 'диагностика иммобилайзера', 'Диагностика и проверка корректности механизма, предотвращающего возможность запуска двигателя Вашего автомобиля третьими лицами. Позволяет выявить все неисправности в работе системы. Осуществляем услуги по проверке соответствия, перепрограммированию устройства, отключению прибора и пр.', 1, '2019-11-05 21:46:42', '2019-11-05 21:46:42'),
-	(9, 'ремонт cтартеров и генераторов', 'Широкий комплекс услуг, современное оборудование, квалифицированные мастера и гарантия качества. Ремонт, срочный ремонт, диагностика, замена, восстановление стартеров и генераторов.', 2, '2019-11-05 21:51:37', '2019-11-05 21:51:37'),
-	(10, 'ремонт стеклоочистителей', 'Устранение неполадок, подбор, замена и наладка работы «дворников» автомобиля, очистка и ремонт составляющих механизмов, влияющих на работу стеклоочистителей.', 2, '2019-11-05 21:51:37', '2019-11-05 21:51:37'),
-	(11, 'замена датчиков', 'Определение работоспособности и технического состояния, диагностика, замена и устранение неисправностей датчиков работы автомобилей квалифицированными специалистами.', 2, '2019-11-05 21:51:37', '2019-11-05 21:51:37'),
-	(12, 'ремонт стеклоподъемников', 'Диагностика и устранение неисправностей в работе механизмов стеклоподъемников автомобиля, коррекция скорости срабатывания, прочистка, устранение шумов и заклинивания при работе, замена деталей устройств нашими опытными квалифицированными мастерами.', 2, '2019-11-05 21:51:37', '2019-11-05 21:51:37');
+INSERT INTO `service_types` (`id`, `title`, `description`, `img_src`, `img_alt`, `fk_service`, `created_at`, `updated_at`) VALUES
+	(1, 'компьютерная диагностика', 'Используя современное оборудование и методики проводим полную диагностику систем автомобиля.', 'storage/diagnostics/diagnostics_1.jpg', 'diagnostics_1', 1, '2019-11-05 21:46:42', '2019-11-05 21:46:42'),
+	(2, 'диагностика двигателя', 'Диагностика работы элементов систем двигателя автомобиля квалифицированными мастерами, определение: — качества работы\r\nмотора, — неисправностей при запуске мотора, — причин падения мощности, — причин посторонних звуков в моторе и пр.', 'storage/diagnostics/diagnostics_2.jpg', 'diagnostics_1', 1, '2019-11-05 21:46:42', '2019-11-08 13:20:53'),
+	(3, 'диагностика АКПП', 'Проводим полную диагностику автоматической коробки передач автомобиля, устанавливаем наличие или отсутствие проблем в работе коробки, консультируем и при выявлении ошибок подбираем наиболее оптимальное решение проблем с последующим устранением.', 'storage/diagnostics/diagnostics_3.jpg', 'diagnostics_1', 1, '2019-11-05 21:46:42', '2019-11-08 13:20:57'),
+	(4, 'диагностика ABS', 'Антиблокировочная система – важная составляющая системы безопасности автомобиля. Наши мастера произведут качественную диагностику системы с определением необходимости ремонта блока ABS. Позаботьтесь о безопасности заранее.', 'storage/diagnostics/diagnostics_4.jpg', 'diagnostics_1', 1, '2019-11-05 21:46:42', '2019-11-08 13:20:59'),
+	(5, 'диагностика Airbag', 'С помощью современной компьютерной диагностики определяем наличие или отсутствие неисправностей в системе работы подушек безопасности Вашего автомобиля. Ремонт, прошивка, удаление ошибок, восстановление работоспособности.', 'storage/diagnostics/diagnostics_5.jpg', 'diagnostics_1', 1, '2019-11-05 21:46:42', '2019-11-08 13:21:03'),
+	(6, 'диагностика климат контроля', 'При помощи спецоборудования производим считывание системы климат контроля Вашего автомобиля, определяем состав, температуру, влажность воздуха в Авто. При выявлении дефекта системы наши сотрудники подберут наиболее рациональное решение по устранению ошибок, проконсультируют и дадут советы для дальнейшей успешной работы системы. (Устранение климатического дискомфорта в салоне).', 'storage/diagnostics/diagnostics_6.jpg', 'diagnostics_1', 1, '2019-11-05 21:46:42', '2019-11-08 13:21:06'),
+	(7, 'диагностика приборной панели', 'Данный тип компьютерной проверки направлен на считывание, расшифровку и анализ полученных значений приборной панели. Проверка показаний спидометра, тахометра, указателей уровня топлива, температуры и пр. показателей, значения которых выводятся на панель приборов.', 'storage/diagnostics/diagnostics_7.jpg', 'diagnostics_1', 1, '2019-11-05 21:46:42', '2019-11-08 13:21:09'),
+	(8, 'диагностика иммобилайзера', 'Диагностика и проверка корректности механизма, предотвращающего возможность запуска двигателя Вашего автомобиля третьими лицами. Позволяет выявить все неисправности в работе системы. Осуществляем услуги по проверке соответствия, перепрограммированию устройства, отключению прибора и пр.', 'storage/diagnostics/diagnostics_8.jpg', 'diagnostics_1', 1, '2019-11-05 21:46:42', '2019-11-08 13:21:12'),
+	(9, 'ремонт cтартеров и генераторов', 'Широкий комплекс услуг, современное оборудование, квалифицированные мастера и гарантия качества. Ремонт, срочный ремонт, диагностика, замена, восстановление стартеров и генераторов.', 'storage/repair/repair_1.png', 'repair_1', 2, '2019-11-05 21:51:37', '2019-11-08 13:24:22'),
+	(10, 'ремонт стеклоочистителей', 'Устранение неполадок, подбор, замена и наладка работы «дворников» автомобиля, очистка и ремонт составляющих механизмов, влияющих на работу стеклоочистителей.', 'storage/repair/repair_2.png', 'repair_1', 2, '2019-11-05 21:51:37', '2019-11-08 13:24:27'),
+	(11, 'замена датчиков', 'Определение работоспособности и технического состояния, диагностика, замена и устранение неисправностей датчиков работы автомобилей квалифицированными специалистами.', 'storage/repair/repair_3.png', 'repair_1', 2, '2019-11-05 21:51:37', '2019-11-08 13:24:31'),
+	(12, 'ремонт стеклоподъемников', 'Диагностика и устранение неисправностей в работе механизмов стеклоподъемников автомобиля, коррекция скорости срабатывания, прочистка, устранение шумов и заклинивания при работе, замена деталей устройств нашими опытными квалифицированными мастерами.', 'storage/repair/repair_4.png', 'repair_1', 2, '2019-11-05 21:51:37', '2019-11-08 13:24:35');
 /*!40000 ALTER TABLE `service_types` ENABLE KEYS */;
 
 -- Дамп структуры для таблица avtoelectrikminsk.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
